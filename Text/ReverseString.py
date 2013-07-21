912a3a2c279faf894aa12856fac21cfd4ad92eb9
Jacob W.E <jj.coolay@gmail.com>
2013-07-21 23:25:28 +0000

    Updating ReadMe

--- Text/ReverseString.py
+++ Text/ReverseString.py
@@ -1,3 +1,11 @@
+#Reverse string program
+#
+#Takes the users input as a var
 rinput = input("Enter your word[s] here: ")
+#prints the var as a string, in a step of -1; aka, reversed.
 print (str(rinput)[::-1])
-
+#
+#This can be done in shell as this:
+#
+#>>> rinput = input("Input: ")
+#>>> str(rinput) [::-1]
