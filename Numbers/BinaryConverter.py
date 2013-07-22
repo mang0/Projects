23417cf81111dbed7000870385af6a5b0743eaf3
Jacob W.E <jj.coolay@gmail.com>
2013-07-22 22:01:32 +0000

    A couple of number ones - Binary calc still WIP

---
+++ Numbers/BinaryConverter.py
@@ -0,0 +1,22 @@
+#Decimal to binary
+def toBinary():
+	rinput = int(input("Enter your decimal number here: " ))
+	print (bin(rinput))
+#Binary to Decimal
+def fromBinary():
+	rinput = int(input("Enter your binary number here: "))
+	print (rinput)
+#Main
+def calc():
+	print ("Enter 'B2D' to convert binary to decimal.")
+	print ("Enter 'D2B' to convert decimal to binary.")
+	rinput = input("Your choice: ")
+	if rinput == 'b2d':
+		fromBinary()
+	if rinput == 'd2b':
+		toBinary()
+	else:
+		print ("Woops! What was that?")
+		calc()
+#begin
+calc()
