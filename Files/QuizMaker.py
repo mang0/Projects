e5dd21196b7136a6205d7dc59a41a1240a216b93
Jacob W.E <jj.coolay@gmail.com>
2013-07-26 01:52:09 +0000

    Updating. Sleep beckons.

--- Files/QuizMaker.py
+++ Files/QuizMaker.py
@@ -3,7 +3,17 @@ import random
 
 print ("This is a quiz. Answer the questions correctly, or die.")
 
-file = open ("Quiz.txt")
-data = file.readlines()
+q_db= open ("Quiz.txt")
+print (type(q_db.read))
+q_db.seek(0)
+data = q_db.read()
 
-print (data)
+q_db.close()
+
+print (data)#with removed *@
+
+#"""Todo list:
+#Data structure stuff
+#questionx.answers or questionx.true/false
+#Then use file data to continue
+#check templateParser.py
